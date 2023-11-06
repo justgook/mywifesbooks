a900c98d256aee3a6648c3a1f688fc3bb7a4119b
Romāns Potašovs <justgook@gmail.com>
2023-11-06 16:46:59 +0200

    🎨 add more images to home page

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,7 +1,24 @@
-const themeDir = __dirname + '/../../';
+const themeDir = __dirname + "/../../"
 export default {
-  content: [`${themeDir}/layouts/**/*.html`, `${themeDir}/content/**/*.md`],
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    "50": "#fdf4ff",
+                    "100": "#fae8ff",
+                    "200": "#f5d0fe",
+                    "300": "#f0abfc",
+                    "400": "#e879f9",
+                    "500": "#d946ef",
+                    "600": "#c026d3",
+                    "700": "#a21caf",
+                    "800": "#86198f",
+                    "900": "#701a75",
+                    "950": "#4a044e",
+                },
+            },
+        },
+    },
+    content: [`${themeDir}/layouts/**/*.html`, `${themeDir}/content/**/*.md`],
+    plugins: [require("@tailwindcss/typography")],
 }
